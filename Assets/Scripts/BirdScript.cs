0d1ea695582c7fb8939ccc02b26edb1ab0de10b9
Ronny <ronnyrit@gmail.com>
2025-05-12 19:35:22 -0700

    Add FlappyBird bird with jump, (i) to switch camera. Organized folders

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D birdRigidBody;
+    public float jumpForce = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            birdRigidBody.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class CameraSwitcher : MonoBehaviour
 {
 
-    public GameObject cam1, cam2;
+    public GameObject cam1, cam2, cam3;
     
     private void Start()
     {
-        cam1.SetActive(true);
+        cam1.SetActive(false);
         cam2.SetActive(false);
+        cam3.SetActive(true);
+
     }
 
     void Update()
@@ -17,11 +19,19 @@ public class CameraSwitcher : MonoBehaviour
         {
             cam1.SetActive(true);
             cam2.SetActive(false);
+            cam3.SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.P))
         {
             cam1.SetActive(false);
             cam2.SetActive(true);
+            cam3.SetActive(false);
+
+        }
+        if (Input.GetKeyDown(KeyCode.I)) {
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(true);
         }
     }
 }
